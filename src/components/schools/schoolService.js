211c140f8c468e8b4fc85868db6c0b700c0c16f2
Mimi <cretu.mihaelaa11@gmail.com>
2022-08-04 12:12:03 +0300

    fix: fixed the api url

--- src/components/schools/schoolService.js
+++ src/components/schools/schoolService.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const API_CREATE_SCHOOL_URL = "http://localhost:8080/api/school/";
-const API_GET_SCHOOLS_URL = "http://localhost:8080/api/schools/";
+const API_SCHOOLS_URL = "http://localhost:8080/api/schools/";
 
 const createSchool = async (schoolData) => {
-  const response = await axios.post(API_CREATE_SCHOOL_URL, schoolData, {
+  const response = await axios.post(API_SCHOOLS_URL, schoolData, {
     headers: {
       // Overwrite Axios's automatically set Content-Type
       "Content-Type": "application/json",
@@ -14,12 +13,12 @@ const createSchool = async (schoolData) => {
 };
 
 const getSchools = async () => {
-  const response = await axios.get(API_GET_SCHOOLS_URL);
+  const response = await axios.get(API_SCHOOLS_URL);
   return response.data;
 };
 
 const getSchoolById = async (schoolId) => {
-  const response = await axios.get(`${API_GET_SCHOOLS_URL}${schoolId}`);
+  const response = await axios.get(`${API_SCHOOLS_URL}${schoolId}`);
   return response.data;
 };
 
